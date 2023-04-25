56c677efde09f430e372f87dd0b904443eaf3e46
chris bruff <84159535+ChrisBruff@users.noreply.github.com>
2023-04-25 16:03:14 -0700

    Initial commit

---
+++ Server.py
@@ -0,0 +1,45 @@
+import threading
+import socket
+host = '127.0.0.1'
+port = 59000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+clients = []
+aliases = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle_client(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index
+            clients.remove(client)
+            client.close()
+            alias = aliases[index]
+            broadcast(f'{alias} has left!'.encode)
+            ('utf-8')
+            aliases.remove(alias)
+
+
+def recieve():
+    while True:
+        print("server is on and ready")
+        client, address = server.accept()
+        print(f'connection made with {str(address)}')
+        client.send('alias?'.encode('utf-8'))
+        alias = client.recv(1024)
+        aliases.append(alias)
+        clients.append(client)
+        print(f'The aliast of the current client {alias}'.encode('utf-8'))
+        broadcast(f'{alias} has connected'.encode('utf-8'))
+        client.send("You have connected".encode('utf-8'))
+
+        thread = threading.Thread(target=handle_client, args=(client,))
+        thread.start()
